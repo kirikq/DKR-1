24320e29983dc758203b994da884744d98ac79d4
kirikq <144107450+kirikq@users.noreply.github.com>
2024-09-26 22:09:56 +0300

    Add files via upload

---
+++ FractalMein.pas
@@ -0,0 +1,49 @@
+﻿Uses GraphABC,FractalModule;
+
+var
+    ScaleFactor : Real = 1; // коэффициент масштабирования
+    XPos, YPos : Integer; // текущие координаты фигуры
+
+
+
+Procedure UpdateFractal(iterations, depth: Integer); //отрисовка фрактал
+Begin
+    ClearWindow;
+    DrawPeano(100 + XPos + 1, 355 + YPos + 1, 400 * ScaleFactor, 0, iterations, depth);
+End;
+
+Begin
+    SetWindowCaption('Фракталы: Кривая Пеано');
+    SetWindowSize(650,500);
+    ClearWindow;
+    
+    var iterations, depth: Integer;
+    repeat
+        Write('Введите количество итераций (0-7): ');
+        Readln(iterations);
+    until (iterations >= 0) and (iterations <= 7);
+    
+    repeat
+        Write('Введите глубину прорисовки (0-7): ');
+        Readln(depth);
+    until (depth >= 0) and (depth <= 7);
+    
+    XPos := 0; 
+    YPos := 0;
+    
+    UpdateFractal(iterations, depth); // отрисовка фрактала с заданными параметрами
+    
+    OnKeyDown := (key: integer) -> // обработчик нажатий клавиш
+    begin
+        case key of
+            VK_A: XPos := XPos - 10; // движение влево
+            VK_D: XPos := XPos + 10; // движение вправо
+            VK_W: YPos := YPos - 10; // движение вверх
+            VK_S: YPos := YPos + 10; // движение вниз
+        end;
+        
+        UpdateFractal(iterations, depth); // обновление фрактала после перемещения
+    end;
+    
+    Readln; // чтобы окно не закрывалось сразу
+End.
---
+++ FractalModule.pas
@@ -0,0 +1,35 @@
+﻿  unit FractalModule;
+
+
+uses GraphABC;
+Procedure DrawPeano(x, y, l, u : Real; t, depth : Integer); //проверка параметров
+
+Procedure Draw2Peano(Var x, y: Real; l, u : Real; t, depth : Integer); //построения фрактала 
+    DrawPeano(x, y, l, u, t, depth);
+    x := x + l*cos(u);
+    y := y - l*sin(u);
+End;
+
+Begin
+    If (t > 0) and (depth > 0) Then //база
+    Begin
+        l := l/3;
+        Draw2Peano(x, y, l, u, t-1, depth-1); //декомпозиция
+        u := u - pi/2;
+        Draw2Peano(x, y, l, u, t-1, depth-1);//декомпозиция
+        u := u + pi/2;
+        Draw2Peano(x, y, l, u, t-1, depth-1);//декомпозиция
+        u := u + pi/2;
+        Draw2Peano(x, y, l, u, t-1, depth-1);//декомпозиция
+        u := u + pi/2;
+        Draw2Peano(x, y, l, u, t-1, depth-1);//декомпозиция
+        u := u - pi/2;
+        Draw2Peano(x, y, l, u, t-1, depth-1);//декомпозиция
+        Draw2Peano(x, y, l*3, u + pi/2, t-1, depth-1); //декомпозиция
+    End
+    Else //база
+        Line(Round(x), Round(y), Round(x + cos(u)*l), Round(y -sin(u)*l));
+
+End;
+begin
+end.
